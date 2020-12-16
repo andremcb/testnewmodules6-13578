bfa6ac5a7a6d57b846e095aea7affd450e298ff9
André Machado <csantos.machado@gmail.com>
2020-12-16 18:40:24 +0000

    Initial commit.

---
+++ ios/testnewmodules6_13578Tests/testnewmodules6_13578Tests.m
@@ -0,0 +1,30 @@
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+#import <React/RCTLog.h>
+#import <React/RCTRootView.h>
+
+#define TIMEOUT_SECONDS 600
+#define TEXT_TO_LOOK_FOR @"Welcome to React"
+
+@interface testnewmodules6_13578 : XCTestCase
+
+@end
+
+@implementation testnewmodules6_13578
+
+- (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
+{
+  if (test(view)) {
+    return YES;
+  }
+  for (UIView *subview in [view subviews]) {
+    if ([self findSubviewInView:subview matching:test]) {
+      return YES;
+    }
+  }
+  return NO;
+}
+
+
+@end
